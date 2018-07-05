47c89fdc9c9b5b70675d0350ca8fc3d4389bc439
Vaughn Vernon <vaughn@forcomprehension.com>
2018-07-04 20:17:47 -0700

    Refactored to simple cached statement (no blob).

---
+++ src/main/java/io/vlingo/symbio/store/state/jdbc/CachedStatement.java
@@ -0,0 +1,18 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.symbio.store.state.jdbc;
+
+import java.sql.PreparedStatement;
+
+public class CachedStatement {
+  public final PreparedStatement preparedStatement;
+
+  public CachedStatement(final PreparedStatement preparedStatement) {
+    this.preparedStatement = preparedStatement;
+  }
+}
