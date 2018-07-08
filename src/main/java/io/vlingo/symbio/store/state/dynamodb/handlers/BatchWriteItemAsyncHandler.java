63127f75db2a1d5c3255c9965efe0540870b7821
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-07-09 00:27:59 +0200

    Basic implementation of read-write on DynamoDB

--- src/main/java/io/vlingo/symbio/store/state/dynamodb/handlers/BatchWriteItemAsyncHandler.java
+++ src/main/java/io/vlingo/symbio/store/state/dynamodb/handlers/BatchWriteItemAsyncHandler.java
@@ -1,4 +1,26 @@
 package io.vlingo.symbio.store.state.dynamodb.handlers;
 
-public class BatchWriteItemAsyncHandler {
+import com.amazonaws.handlers.AsyncHandler;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemRequest;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemResult;
+import io.vlingo.symbio.State;
+import io.vlingo.symbio.store.state.StateStore;
+
+public class BatchWriteItemAsyncHandler implements AsyncHandler<BatchWriteItemRequest, BatchWriteItemResult> {
+    private final State<String> state;
+    private final StateStore.WriteResultInterest<String> interest;
+
+    public BatchWriteItemAsyncHandler(State<String> state, StateStore.WriteResultInterest<String> interest) {
+        this.state = state;
+        this.interest = interest;
+    }
+
+    @Override
+    public void onError(Exception e) {
+    }
+
+    @Override
+    public void onSuccess(BatchWriteItemRequest request, BatchWriteItemResult batchWriteItemResult) {
+        interest.writeResultedIn(StateStore.Result.Success, state.id, state);
+    }
 }
--- src/main/java/io/vlingo/symbio/store/state/dynamodb/handlers/GetItemAsyncHandler.java
+++ src/main/java/io/vlingo/symbio/store/state/dynamodb/handlers/GetItemAsyncHandler.java
@@ -1,4 +1,38 @@
 package io.vlingo.symbio.store.state.dynamodb.handlers;
 
-public class GetItemAsyncHandler {
+import com.amazonaws.handlers.AsyncHandler;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.GetItemResult;
+import io.vlingo.symbio.State;
+import io.vlingo.symbio.store.state.StateStore;
+import io.vlingo.symbio.store.state.dynamodb.StateRecordAdapter;
+
+import java.util.Map;
+
+public class GetItemAsyncHandler implements AsyncHandler<GetItemRequest, GetItemResult> {
+    private final StateStore.ReadResultInterest<String> interest;
+
+    public GetItemAsyncHandler(StateStore.ReadResultInterest<String> interest) {
+        this.interest = interest;
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    @Override
+    public void onSuccess(GetItemRequest request, GetItemResult getItemResult) {
+        Map<String, AttributeValue> item = getItemResult.getItem();
+        try {
+            State<String> state = StateRecordAdapter.unmarshall(item);
+            interest.readResultedIn(StateStore.Result.Success, state.id, state);
+        } catch (ClassNotFoundException e) {
+            interest.readResultedIn(StateStore.Result.Failure,
+                    StateRecordAdapter.unmarshallForId(item),
+                    (State) new State.NullState()
+            );
+        }
+    }
 }
