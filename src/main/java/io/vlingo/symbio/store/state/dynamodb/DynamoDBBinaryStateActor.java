a45a5c2f3dbcde95e67c1344589b3476654c8bc2
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-07-24 22:52:58 +0200

    Implementation of binary state actor (needs refactor)

--- src/main/java/io/vlingo/symbio/store/state/dynamodb/DynamoDBBinaryStateActor.java
+++ src/main/java/io/vlingo/symbio/store/state/dynamodb/DynamoDBBinaryStateActor.java
@@ -3,11 +3,12 @@ package io.vlingo.symbio.store.state.dynamodb;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
 import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.state.BinaryStateStore;
+import io.vlingo.symbio.store.state.dynamodb.adapters.BinaryStateRecordAdapter;
 import io.vlingo.symbio.store.state.dynamodb.interests.CreateTableInterest;
 
 public class DynamoDBBinaryStateActor extends DynamoDBStateActor<byte[]> implements BinaryStateStore {
     public DynamoDBBinaryStateActor(Dispatcher dispatcher, AmazonDynamoDBAsync dynamodb, CreateTableInterest createTableInterest) {
-        super(dispatcher, dynamodb, createTableInterest, null, State.NullState.Binary);
+        super(dispatcher, dynamodb, createTableInterest, new BinaryStateRecordAdapter(), State.NullState.Binary);
 
         createTableInterest.createDispatchableTable(dynamodb, DISPATCHABLE_TABLE_NAME);
     }
---
+++ src/test/java/io/vlingo/symbio/store/state/dynamodb/DynamoDBBinaryStateActorTest.java
@@ -0,0 +1,72 @@
+package io.vlingo.symbio.store.state.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import io.vlingo.actors.Definition;
+import io.vlingo.actors.Protocols;
+import io.vlingo.actors.World;
+import io.vlingo.symbio.Metadata;
+import io.vlingo.symbio.State;
+import io.vlingo.symbio.store.state.BinaryStateStore;
+import io.vlingo.symbio.store.state.Entity1;
+import io.vlingo.symbio.store.state.StateStore;
+import io.vlingo.symbio.store.state.TextStateStore;
+import io.vlingo.symbio.store.state.dynamodb.adapters.BinaryStateRecordAdapter;
+import io.vlingo.symbio.store.state.dynamodb.adapters.RecordAdapter;
+import io.vlingo.symbio.store.state.dynamodb.adapters.TextStateRecordAdapter;
+import io.vlingo.symbio.store.state.dynamodb.interests.CreateTableInterest;
+
+import java.util.UUID;
+
+public class DynamoDBBinaryStateActorTest extends DynamoDBStateActorTest<BinaryStateStore, byte[]> {
+    @Override
+    protected Protocols stateStoreProtocols(World world, StateStore.Dispatcher dispatcher, AmazonDynamoDBAsync dynamodb, CreateTableInterest interest) {
+        return world.actorFor(
+                Definition.has(DynamoDBBinaryStateActor.class, Definition.parameters(dispatcher, dynamodb, interest)),
+                new Class[]{State.BinaryState.class, StateStore.DispatcherControl.class}
+        );
+    }
+
+    @Override
+    protected void doWrite(BinaryStateStore actor, State<byte[]> state, StateStore.WriteResultInterest<byte[]> interest) {
+        actor.write(state, interest);
+    }
+
+    @Override
+    protected void doRead(BinaryStateStore actor, String id, Class<?> type, StateStore.ReadResultInterest<byte[]> interest) {
+        actor.read(id, type, interest);
+    }
+
+    @Override
+    protected State<byte[]> nullState() {
+        return State.NullState.Binary;
+    }
+
+    @Override
+    protected State<byte[]> randomState() {
+        return new State.BinaryState(
+                UUID.randomUUID().toString(),
+                Entity1.class,
+                1,
+                UUID.randomUUID().toString().getBytes(),
+                1,
+                new Metadata(UUID.randomUUID().toString(), UUID.randomUUID().toString())
+        );
+    }
+
+    @Override
+    protected State<byte[]> newFor(State<byte[]> oldState) {
+        return new State.BinaryState(
+                oldState.id,
+                oldState.typed(),
+                oldState.typeVersion,
+                oldState.data,
+                oldState.dataVersion + 1,
+                oldState.metadata
+        );
+    }
+
+    @Override
+    protected RecordAdapter<byte[]> recordAdapter() {
+        return new BinaryStateRecordAdapter();
+    }
+}
