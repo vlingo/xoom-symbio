410a3d13159016b8aded1bbf3e9ad44782041fc0
Vaughn Vernon <vaughn@forcomprehension.com>
2018-07-29 16:19:17 -0700

    Moved state store dynamodb and jdbc impls to dedicated projects.

--- src/main/java/io/vlingo/symbio/store/state/dynamodb/interests/CreateTableInterest.java
+++
@@ -1,9 +0,0 @@
-package io.vlingo.symbio.store.state.dynamodb.interests;
-
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
-
-public interface CreateTableInterest {
-    void createDispatchableTable(AmazonDynamoDBAsync dynamoDBAsync, String tableName);
-
-    void createEntityTable(AmazonDynamoDBAsync dynamoDBAsync, String tableName);
-}
