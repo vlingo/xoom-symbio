b538adcbac49ac3575ef059e56362ee67d5ee554
Vaughn Vernon <vaughn@forcomprehension.com>
2018-10-29 21:01:48 -0700

    Migrated Result.

--- src/main/java/io/vlingo/symbio/store/state/StateStoreReadResultInterest__Proxy.java
+++ src/main/java/io/vlingo/symbio/store/state/StateStoreReadResultInterest__Proxy.java
@@ -10,8 +10,8 @@ import io.vlingo.symbio.store.state.StateStore.ReadResultInterest;
 
 public class StateStoreReadResultInterest__Proxy<T> implements ReadResultInterest<T> {
 
-  private static final String readResultedInRepresentation1 = "readResultedIn(io.vlingo.symbio.store.state.StateStore.Result, java.lang.String, io.vlingo.symbio.State<T>)";
-  private static final String readResultedInRepresentation2 = "readResultedIn(io.vlingo.symbio.store.state.StateStore.Result, java.lang.Exception, java.lang.String, io.vlingo.symbio.State<T>)";
+  private static final String readResultedInRepresentation1 = "readResultedIn(io.vlingo.symbio.store.Result, java.lang.String, io.vlingo.symbio.State<T>)";
+  private static final String readResultedInRepresentation2 = "readResultedIn(io.vlingo.symbio.store.Result, java.lang.Exception, java.lang.String, io.vlingo.symbio.State<T>)";
 
   private final Actor actor;
   private final Mailbox mailbox;
@@ -22,7 +22,7 @@ public class StateStoreReadResultInterest__Proxy<T> implements ReadResultInteres
   }
 
   @SuppressWarnings({ "rawtypes", "unchecked" })
-  public void readResultedIn(io.vlingo.symbio.store.state.StateStore.Result arg0, java.lang.String arg1, io.vlingo.symbio.State<T> arg2, final Object arg3) {
+  public void readResultedIn(io.vlingo.symbio.store.Result arg0, java.lang.String arg1, io.vlingo.symbio.State<T> arg2, final Object arg3) {
     if (!actor.isStopped()) {
       final Consumer<ReadResultInterest<T>> consumer = (actor) -> actor.readResultedIn(arg0, arg1, arg2, arg3);
       mailbox.send(new LocalMessage(actor, ReadResultInterest.class, consumer, readResultedInRepresentation1));
@@ -31,7 +31,7 @@ public class StateStoreReadResultInterest__Proxy<T> implements ReadResultInteres
     }
   }
   @SuppressWarnings({ "rawtypes", "unchecked" })
-  public void readResultedIn(io.vlingo.symbio.store.state.StateStore.Result arg0, java.lang.Exception arg1, java.lang.String arg2, io.vlingo.symbio.State<T> arg3, final Object arg4) {
+  public void readResultedIn(io.vlingo.symbio.store.Result arg0, java.lang.Exception arg1, java.lang.String arg2, io.vlingo.symbio.State<T> arg3, final Object arg4) {
     if (!actor.isStopped()) {
       final Consumer<ReadResultInterest<T>> consumer = (actor) -> actor.readResultedIn(arg0, arg1, arg2, arg3, arg4);
       mailbox.send(new LocalMessage(actor, ReadResultInterest.class, consumer, readResultedInRepresentation2));
