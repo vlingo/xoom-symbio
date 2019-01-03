76a2deacf1dd52394bf95078d9e940e65d10ad6a
Vaughn Vernon <vaughn@forcomprehension.com>
2019-01-03 16:44:22 -0700

    Added standard methods and adapters.

--- src/test/java/io/vlingo/symbio/store/state/Entity1.java
+++ src/test/java/io/vlingo/symbio/store/state/Entity1.java
@@ -7,6 +7,11 @@
 
 package io.vlingo.symbio.store.state;
 
+import io.vlingo.common.serialization.JsonSerialization;
+import io.vlingo.symbio.Metadata;
+import io.vlingo.symbio.State.TextState;
+import io.vlingo.symbio.StateAdapter;
+
 public class Entity1 {
   public final String id;
   public final int value;
@@ -17,7 +22,44 @@ public class Entity1 {
   }
 
   @Override
+  public int hashCode() {
+    return 31 * id.hashCode();
+  }
+
+  @Override
+  public boolean equals(final Object other) {
+    if (other == null || other.getClass() != this.getClass()) {
+      return false;
+    }
+    return this.id.equals(((Entity1) other).id);
+  }
+
+  @Override
   public String toString() {
     return "Entity1[id=" + id + " value=" + value + "]";
+  }
+
+  public static class Entity1StateAdapter implements StateAdapter<Entity1,TextState> {
+
+    @Override
+    public int typeVersion() {
+      return 1;
+    }
+
+    @Override
+    public Entity1 fromRawState(final TextState raw) {
+      return JsonSerialization.deserialized(raw.data, raw.typed());
+    }
+
+    @Override
+    public TextState toRawState(final Entity1 state, final int stateVersion) {
+      return toRawState(state, stateVersion, Metadata.with("value", "op"));
+    }
+
+    @Override
+    public TextState toRawState(final Entity1 state, final int stateVersion, final Metadata metadata) {
+      final String serialization = JsonSerialization.serialized(state);
+      return new TextState(state.id, Entity1.class, typeVersion(), serialization, stateVersion, metadata);
+    }
   }
 }
