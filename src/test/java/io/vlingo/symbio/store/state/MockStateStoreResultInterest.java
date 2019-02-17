a4499420b3fdeb906f107d1949e00b7bbbcc5cc8
VaughnVernon <vaughn@forcomprehension.com>
2019-02-17 05:55:08 -0700

    Refactored to AccessSafely.

--- src/test/java/io/vlingo/symbio/store/state/MockStateStoreResultInterest.java
+++ src/test/java/io/vlingo/symbio/store/state/MockStateStoreResultInterest.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.actors.testkit.AccessSafely;
 import io.vlingo.common.Outcome;
 import io.vlingo.symbio.Metadata;
 import io.vlingo.symbio.store.Result;
@@ -25,10 +25,11 @@ public class MockStateStoreResultInterest
                WriteResultInterest,
                ConfirmDispatchedResultInterest {
 
+  private AccessSafely access;
+
   public AtomicInteger confirmDispatchedResultedIn = new AtomicInteger(0);
   public AtomicInteger readObjectResultedIn = new AtomicInteger(0);
   public AtomicInteger writeObjectResultedIn = new AtomicInteger(0);
-  public TestUntil until;
 
   public AtomicReference<Result> objectReadResult = new AtomicReference<>();
   public AtomicReference<Result> objectWriteResult = new AtomicReference<>();
@@ -37,34 +38,22 @@ public class MockStateStoreResultInterest
   public AtomicReference<Object> objectState = new AtomicReference<>();
   public ConcurrentLinkedQueue<Exception> errorCauses = new ConcurrentLinkedQueue<>();
 
-  public MockStateStoreResultInterest(final int testUntilHappenings) {
-    until = TestUntil.happenings(testUntilHappenings);
-  }
+  public MockStateStoreResultInterest() { }
 
   @Override
   public void confirmDispatchedResultedIn(final Result result, final String dispatchId) {
-    confirmDispatchedResultedIn.incrementAndGet();
-    until.happened();
+    // not used
   }
 
   @Override
   public <S> void readResultedIn(final Outcome<StorageException, Result> outcome, final String id, final S state, final int stateVersion, final Metadata metadata, final Object object) {
     outcome
       .andThen(result -> {
-        readObjectResultedIn.incrementAndGet();
-        objectReadResult.set(result);
-        objectState.set(state);
-        metadataHolder.set(metadata);
-        until.happened();
+        access.writeUsing("readStoreData", new StoreData(1, result, state, metadata, null));
         return result;
       })
       .otherwise(cause -> {
-        readObjectResultedIn.incrementAndGet();
-        objectReadResult.set(cause.result);
-        objectState.set(state);
-        metadataHolder.set(metadata);
-        errorCauses.add(cause);
-        until.happened();
+        access.writeUsing("readStoreData", new StoreData(1, cause.result, state, metadata, cause));
         return cause.result;
       });
   }
@@ -73,21 +62,70 @@ public class MockStateStoreResultInterest
   public <S> void writeResultedIn(final Outcome<StorageException, Result> outcome, final String id, final S state, final int stateVersion, final Object object) {
     outcome
       .andThen(result -> {
-        writeObjectResultedIn.incrementAndGet();
-        objectWriteResult.set(result);
-        objectWriteAccumulatedResults.add(result);
-        objectState.set(state);
-        until.happened();
+        access.writeUsing("writeStoreData", new StoreData(1, result, state, null, null));
         return result;
       })
       .otherwise(cause -> {
-        writeObjectResultedIn.incrementAndGet();
-        objectWriteResult.set(cause.result);
-        objectWriteAccumulatedResults.add(cause.result);
-        objectState.set(state);
-        errorCauses.add(cause);
-        until.happened();
+        access.writeUsing("writeStoreData", new StoreData(1, cause.result, state, null, cause));
         return cause.result;
       });
+  }
+
+  public AccessSafely afterCompleting(final int times) {
+    access = AccessSafely.afterCompleting(times);
+
+    access
+      .writingWith("confirmDispatchedResultedIn", (Integer increment) -> confirmDispatchedResultedIn.addAndGet(increment))
+      .readingWith("confirmDispatchedResultedIn", () -> confirmDispatchedResultedIn.get())
+
+      .writingWith("writeStoreData", (StoreData data) -> {
+        writeObjectResultedIn.addAndGet(data.resultedIn);
+        objectWriteResult.set(data.result);
+        objectWriteAccumulatedResults.add(data.result);
+        objectState.set(data.state);
+        metadataHolder.set(data.metadata);
+        if (data.errorCauses != null) {
+          errorCauses.add(data.errorCauses);
+        }
+      })
+      .writingWith("readStoreData", (StoreData data) -> {
+        readObjectResultedIn.addAndGet(data.resultedIn);
+        objectReadResult.set(data.result);
+        objectWriteAccumulatedResults.add(data.result);
+        objectState.set(data.state);
+        metadataHolder.set(data.metadata);
+        if (data.errorCauses != null) {
+          errorCauses.add(data.errorCauses);
+        }
+      })
+
+      .readingWith("readObjectResultedIn", () -> readObjectResultedIn.get())
+      .readingWith("objectReadResult", () -> objectReadResult.get())
+      .readingWith("objectWriteResult", () -> objectWriteResult.get())
+      .readingWith("objectWriteAccumulatedResults", () -> objectWriteAccumulatedResults.poll())
+      .readingWith("objectWriteAccumulatedResultsCount", () -> objectWriteAccumulatedResults.size())
+      .readingWith("metadataHolder", () -> metadataHolder.get())
+      .readingWith("objectState", () -> objectState.get())
+      .readingWith("errorCauses", () -> errorCauses.poll())
+      .readingWith("errorCausesCount", () -> errorCauses.size())
+      .readingWith("writeObjectResultedIn", () -> writeObjectResultedIn.get());
+
+    return access;
+  }
+
+  public class StoreData {
+    public final Exception errorCauses;
+    public final Metadata metadata;
+    public final Result result;
+    public final Object state;
+    public final int resultedIn;
+
+    public StoreData(final int resultedIn, final Result objectResult, final Object state, final Metadata metadata, final Exception errorCauses) {
+      this.resultedIn = resultedIn;
+      this.result = objectResult;
+      this.state = state;
+      this.metadata = metadata;
+      this.errorCauses = errorCauses;
+    }
   }
 }
