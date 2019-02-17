483620047ccc7ebd488811073e82855ac7451905
VaughnVernon <vaughn@forcomprehension.com>
2019-02-17 05:54:51 -0700

    Refactored to AccessSafely.

--- src/test/java/io/vlingo/symbio/store/state/MockDispatcher.java
+++ src/test/java/io/vlingo/symbio/store/state/MockDispatcher.java
@@ -11,22 +11,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.actors.testkit.AccessSafely;
 import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.state.StateStore.ConfirmDispatchedResultInterest;
 import io.vlingo.symbio.store.state.StateStore.Dispatcher;
 import io.vlingo.symbio.store.state.StateStore.DispatcherControl;
 
 public class MockDispatcher implements Dispatcher {
-  public final ConfirmDispatchedResultInterest confirmDispatchedResultInterest;
-  public DispatcherControl control;
-  public final Map<String,State<?>> dispatched = new HashMap<>();
-  public final AtomicBoolean processDispatch = new AtomicBoolean(true);
-  public TestUntil until = TestUntil.happenings(0);
+  private AccessSafely access;
 
-  public MockDispatcher(final int testUntilHappenings, final ConfirmDispatchedResultInterest confirmDispatchedResultInterest) {
-    this.until = TestUntil.happenings(testUntilHappenings);
+  private final ConfirmDispatchedResultInterest confirmDispatchedResultInterest;
+  private DispatcherControl control;
+  private final Map<String,Object> dispatched = new HashMap<>();
+  private final AtomicBoolean processDispatch = new AtomicBoolean(true);
+
+  public MockDispatcher(final ConfirmDispatchedResultInterest confirmDispatchedResultInterest) {
     this.confirmDispatchedResultInterest = confirmDispatchedResultInterest;
+    this.access = AccessSafely.afterCompleting(0);
   }
 
   @Override
@@ -37,9 +38,37 @@ public class MockDispatcher implements Dispatcher {
   @Override
   public <S extends State<?>> void dispatch(final String dispatchId, final S state) {
     if (processDispatch.get()) {
-      dispatched.put(dispatchId, state);
+      access.writeUsing("dispatchedState", dispatchId, (State<?>) state);
       control.confirmDispatched(dispatchId, confirmDispatchedResultInterest);
-      until.happened();
     }
+  }
+
+  public AccessSafely afterCompleting(final int times) {
+    access = AccessSafely
+              .afterCompleting(times)
+              .writingWith("dispatchedState", (String id, Object state) -> dispatched.put(id, state))
+              .readingWith("dispatchedState", (String id) -> dispatched.get(id))
+              .readingWith("dispatchedStateCount", () -> dispatched.size())
+
+              .writingWith("processDispatch", (Boolean flag) -> processDispatch.set(flag))
+              .readingWith("processDispatch", () -> processDispatch.get())
+
+              .readingWith("dispatched", () -> dispatched);
+
+    return access;
+  }
+
+  public State<?> dispatched(final String id) {
+    final State<?> dispatched = access.readFrom("dispatchedState", id);
+    return dispatched;
+  }
+
+  public int dispatchedCount() {
+    Map<String,State<?>> dispatched = access.readFrom("dispatched");
+    return dispatched.size();
+  }
+
+  public void dispatchUnconfirmed() {
+    control.dispatchUnconfirmed();
   }
 }
