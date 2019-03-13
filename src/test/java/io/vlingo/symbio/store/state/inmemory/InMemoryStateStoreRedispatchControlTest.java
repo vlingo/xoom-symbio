f2c4f1d60e23daa7958bc5a40deaaabe0c667e05
Dave Muirhead <dave@brsg.biz>
2019-03-12 20:48:23 -0400

    cut the sleep from 6 to 3 seconds

--- src/test/java/io/vlingo/symbio/store/state/inmemory/InMemoryStateStoreRedispatchControlTest.java
+++ src/test/java/io/vlingo/symbio/store/state/inmemory/InMemoryStateStoreRedispatchControlTest.java
@@ -44,7 +44,7 @@ public class InMemoryStateStoreRedispatchControlTest {
     store.write(entity.id, entity, 1, interest);
 
     try {
-      Thread.sleep(6000);
+      Thread.sleep(3000);
     }
     catch (InterruptedException ex) {
       //ignored
