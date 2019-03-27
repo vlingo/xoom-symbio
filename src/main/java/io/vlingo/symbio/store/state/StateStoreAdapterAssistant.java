5bcda60da52ca45d8af053fd8cf3422496f09b15
VaughnVernon <vaughn@forcomprehension.com>
2019-03-27 08:20:55 -0700

    Encorporate DefaultTextStateAdapter.

--- src/main/java/io/vlingo/symbio/store/state/StateStoreAdapterAssistant.java
+++ src/main/java/io/vlingo/symbio/store/state/StateStoreAdapterAssistant.java
@@ -10,35 +10,54 @@ package io.vlingo.symbio.store.state;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.vlingo.symbio.DefaultTextStateAdapter;
 import io.vlingo.symbio.Metadata;
 import io.vlingo.symbio.State;
+import io.vlingo.symbio.State.TextState;
 import io.vlingo.symbio.StateAdapter;
 
 public class StateStoreAdapterAssistant {
   private final Map<Class<?>,StateAdapter<?,?>> stateAdapters;
+  private final DefaultTextStateAdapter defaultTextStateAdapter;
 
   public StateStoreAdapterAssistant() {
     this.stateAdapters = new HashMap<>();
+    this.defaultTextStateAdapter = new DefaultTextStateAdapter();
   }
 
   @SuppressWarnings("unchecked")
   public <ST,RS extends State<?>> RS adaptToRawState(final ST state, final int stateVersion) {
     final StateAdapter<ST,RS> adapter = (StateAdapter<ST,RS>) stateAdapters.get(state.getClass());
-    final RS raw = adapter.toRawState(state, stateVersion);
+    final RS raw;
+    if (adapter != null) {
+      raw = adapter.toRawState(state, stateVersion);
+    } else {
+      raw = (RS) defaultTextStateAdapter.toRawState(state, stateVersion);
+    }
     return raw;
   }
 
   @SuppressWarnings("unchecked")
   public <ST,RS extends State<?>> RS adaptToRawState(final ST state, final int stateVersion, final Metadata metadata) {
     final StateAdapter<ST,RS> adapter = (StateAdapter<ST,RS>) stateAdapters.get(state.getClass());
-    final RS raw = adapter.toRawState(state, stateVersion, metadata);
+    final RS raw;
+    if (adapter != null) {
+      raw = adapter.toRawState(state, stateVersion, metadata);
+    } else {
+      raw = (RS) defaultTextStateAdapter.toRawState(state, stateVersion, metadata);
+    }
     return raw;
   }
 
   @SuppressWarnings("unchecked")
   public <ST,RS extends State<?>> ST adaptFromRawState(final RS raw) {
     final StateAdapter<ST,RS> adapter = (StateAdapter<ST,RS>) stateAdapters.get(raw.typed());
-    final ST state = adapter.fromRawState(raw);
+    final ST state;
+    if (adapter != null) {
+      state = adapter.fromRawState(raw);
+    } else {
+      state = (ST) defaultTextStateAdapter.fromRawState((TextState) raw);
+    }
     return state;
   }
 
