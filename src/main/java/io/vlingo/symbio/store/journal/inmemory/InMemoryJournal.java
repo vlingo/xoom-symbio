456465dedaad1abcf65cd23b3e89591395329863
VaughnVernon <vaughn@forcomprehension.com>
2019-04-22 01:28:46 -0700

    Removed adapter registration methods and use adapter providers.

--- src/main/java/io/vlingo/symbio/store/journal/inmemory/InMemoryJournal.java
+++ src/main/java/io/vlingo/symbio/store/journal/inmemory/InMemoryJournal.java
@@ -13,14 +13,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import io.vlingo.actors.World;
 import io.vlingo.common.Completes;
 import io.vlingo.common.Success;
 import io.vlingo.symbio.BaseEntry;
 import io.vlingo.symbio.Entry;
-import io.vlingo.symbio.EntryAdapter;
+import io.vlingo.symbio.EntryAdapterProvider;
 import io.vlingo.symbio.Source;
 import io.vlingo.symbio.State;
-import io.vlingo.symbio.StateAdapter;
+import io.vlingo.symbio.StateAdapterProvider;
 import io.vlingo.symbio.store.Result;
 import io.vlingo.symbio.store.journal.Journal;
 import io.vlingo.symbio.store.journal.JournalListener;
@@ -28,8 +29,8 @@ import io.vlingo.symbio.store.journal.JournalReader;
 import io.vlingo.symbio.store.journal.StreamReader;
 
 public class InMemoryJournal<T,RS extends State<?>> implements Journal<T> {
-  private final Map<Class<?>,EntryAdapter<? extends Source<?>,? extends Entry<?>>> entryAdapters;
-  private final Map<Class<?>,StateAdapter<?,?>> stateAdapters;
+  private final EntryAdapterProvider entryAdapterProvider;
+  private final StateAdapterProvider stateAdapterProvider;
   private final List<Entry<T>> journal;
   private final JournalListener<T> listener;
   private final Map<String,JournalReader<? extends Entry<?>>> journalReaders;
@@ -37,10 +38,10 @@ public class InMemoryJournal<T,RS extends State<?>> implements Journal<T> {
   private final Map<String, Map<Integer,Integer>> streamIndexes;
   private final Map<String,RS> snapshots;
 
-  public InMemoryJournal(final JournalListener<T> listener) {
+  public InMemoryJournal(final JournalListener<T> listener, final World world) {
     this.listener = listener;
-    this.entryAdapters = new HashMap<>();
-    this.stateAdapters = new HashMap<>();
+    this.entryAdapterProvider = EntryAdapterProvider.instance(world);
+    this.stateAdapterProvider = StateAdapterProvider.instance(world);
     this.journal = new ArrayList<>();
     this.journalReaders = new HashMap<>(1);
     this.streamReaders = new HashMap<>(1);
@@ -50,7 +51,7 @@ public class InMemoryJournal<T,RS extends State<?>> implements Journal<T> {
 
   @Override
   public <S,ST> void append(final String streamName, final int streamVersion, final Source<S> source, final AppendResultInterest interest, final Object object) {
-    final Entry<T> entry = asEntry(source);
+    final Entry<T> entry = entryAdapterProvider.asEntry(source);
     insert(streamName, streamVersion, entry);
     listener.appended(entry);
     interest.appendResultedIn(Success.of(Result.Success), streamName, streamVersion, source, Optional.empty(), object);
@@ -59,13 +60,12 @@ public class InMemoryJournal<T,RS extends State<?>> implements Journal<T> {
   @Override
   @SuppressWarnings("unchecked")
   public <S,ST> void appendWith(final String streamName, final int streamVersion, final Source<S> source, final ST snapshot, final AppendResultInterest interest, final Object object) {
-    final Entry<T> entry = asEntry(source);
+    final Entry<T> entry = entryAdapterProvider.asEntry(source);
     insert(streamName, streamVersion, entry);
     final RS raw;
     final Optional<ST> snapshotResult;
     if (snapshot != null) {
-      final StateAdapter<ST,RS> adapter = (StateAdapter<ST,RS>) stateAdapters.get(snapshot.getClass());
-      raw = adapter.toRawState(snapshot, streamVersion);
+      raw = stateAdapterProvider.asRaw(streamName, snapshot, streamVersion);
       snapshots.put(streamName, raw);
       snapshotResult = Optional.of(snapshot);
     } else {
@@ -78,7 +78,7 @@ public class InMemoryJournal<T,RS extends State<?>> implements Journal<T> {
 
   @Override
   public <S,ST> void appendAll(final String streamName, final int fromStreamVersion, final List<Source<S>> sources, final AppendResultInterest interest, final Object object) {
-    final List<Entry<T>> entries = asEntries(sources);
+    final List<Entry<T>> entries = entryAdapterProvider.asEntries(sources);
     insert(streamName, fromStreamVersion, entries);
     listener.appendedAll(entries);
     interest.appendAllResultedIn(Success.of(Result.Success), streamName, fromStreamVersion, sources, Optional.empty(), object);
@@ -87,13 +87,12 @@ public class InMemoryJournal<T,RS extends State<?>> implements Journal<T> {
   @Override
   @SuppressWarnings("unchecked")
   public <S,ST> void appendAllWith(final String streamName, final int fromStreamVersion, final List<Source<S>> sources, final ST snapshot, final AppendResultInterest interest, final Object object) {
-    final List<Entry<T>> entries = asEntries(sources);
+    final List<Entry<T>> entries = entryAdapterProvider.asEntries(sources);
     insert(streamName, fromStreamVersion, entries);
     final RS raw;
     final Optional<ST> snapshotResult;
     if (snapshot != null) {
-      final StateAdapter<ST,RS> adapter = (StateAdapter<ST,RS>) stateAdapters.get(snapshot.getClass());
-      raw = adapter.toRawState(snapshot, fromStreamVersion);
+      raw = stateAdapterProvider.asRaw(streamName, snapshot, fromStreamVersion);
       snapshots.put(streamName, raw);
       snapshotResult = Optional.of(snapshot);
     } else {
@@ -124,40 +123,6 @@ public class InMemoryJournal<T,RS extends State<?>> implements Journal<T> {
       streamReaders.put(name, reader);
     }
     return Completes.withSuccess(reader);
-  }
-
-  @Override
-  public <S extends Source<?>,E extends Entry<?>> void registerEntryAdapter(final Class<S> sourceType, final EntryAdapter<S,E> adapter) {
-    entryAdapters.put(sourceType, adapter);
-  }
-
-  @Override
-  public <S,R extends State<?>> void registerStateAdapter(final Class<S> stateType, final StateAdapter<S,R> adapter) {
-    stateAdapters.put(stateType, adapter);
-  }
-
-  @SuppressWarnings("unchecked")
-  private <S extends Source<?>,E extends Entry<?>> EntryAdapter<S,E> adapter(final Class<S> sourceType) {
-    final EntryAdapter<S,E> adapter = (EntryAdapter<S,E>) entryAdapters.get(sourceType);
-    if (adapter != null) {
-      return adapter;
-    }
-    throw new IllegalStateException("Adapter not registrered for: " + sourceType.getName());
-  }
-
-  private <S> List<Entry<T>> asEntries(final List<Source<S>> sources) {
-    final List<Entry<T>> entries = new ArrayList<>();
-    for (final Source<?> source : sources) {
-      entries.add(asEntry(source));
-    }
-    return entries;
-  }
-
-  @SuppressWarnings("unchecked")
-  private Entry<T> asEntry(final Source<?> source) {
-    final EntryAdapter<Source<?>,Entry<?>>  adapter = (EntryAdapter<Source<?>,Entry<?>>) adapter(source.getClass());
-
-    return (Entry<T>) adapter.toEntry(source);
   }
 
   private void insert(final String streamName, final int streamVersion, final Entry<T> entry) {
