15d3f33a081cd309766fc1ca7d8177a438329dd2
Dave Muirhead <dave@brsg.biz>
2019-04-29 14:42:33 -0400

    added equals and hashCode based to PersistentObject

--- src/main/java/io/vlingo/symbio/store/object/PersistentObject.java
+++ src/main/java/io/vlingo/symbio/store/object/PersistentObject.java
@@ -96,4 +96,28 @@ public abstract class PersistentObject implements Serializable {
    */
   protected PersistentObject() {
   }
+
+  /* @see java.lang.Object#hashCode() */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (int) (persistenceId ^ (persistenceId >>> 32));
+    return result;
+  }
+
+  /* @see java.lang.Object#equals(java.lang.Object) */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    PersistentObject other = (PersistentObject) obj;
+    if (persistenceId != other.persistenceId)
+      return false;
+    return true;
+  }
 }
