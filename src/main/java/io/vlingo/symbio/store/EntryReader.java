e23ef25dea29a7f5512c2f4da28ccb9b04a98390
VaughnVernon <vaughn@forcomprehension.com>
2019-07-29 17:58:00 -0700

    Define and implement default readNext(final String fromId) and readNext(final String fromId, final int maximumEntries).

--- src/main/java/io/vlingo/symbio/store/EntryReader.java
+++ src/main/java/io/vlingo/symbio/store/EntryReader.java
@@ -68,6 +68,23 @@ public interface EntryReader<T extends Entry<?>> {
   Completes<T> readNext();
 
   /**
+   * Eventually answers the next available {@code Entry<T>} instance or null if none is currently available.
+   * The next {@code Entry<T>} instance is relative to the one previously read by the same reader
+   * instance, or the first {@code Entry<T>} instance in the storage if none have previously
+   * been read. Note that this is the least efficient read, because only one {@code Entry<T>} will
+   * be answered, but it may be useful for test purposes or a storage that is
+   * appended to slowly.
+   *
+   * @param fromId the String id of the {@code Entry<T>} instance to which the seek prepares to next read
+   *
+   * @return the {@code Completes<T>} next available entry or null if none
+   */
+  default Completes<T> readNext(final String fromId) {
+    seekTo(fromId);
+    return readNext();
+  }
+
+  /**
    * Eventually answers the next available {@code Entry<T>} instances as a {@code List}, which may be
    * empty if none are currently available. The next {@code Entry<T>} instances are relative to the one(s)
    * previously read by the same reader instance, or the first {@code Entry<T>} instance in the storage
@@ -81,6 +98,25 @@ public interface EntryReader<T extends Entry<?>> {
    * @return the {@code Completes<List<T>>} of at most maximumEntries or empty if none
    */
   Completes<List<T>> readNext(final int maximumEntries);
+
+  /**
+   * Eventually answers the next available {@code Entry<T>} instances as a {@code List}, which may be
+   * empty if none are currently available. The next {@code Entry<T>} instances are relative to the one(s)
+   * previously read by the same reader instance, or the first {@code Entry<T>} instance in the storage
+   * if none have previously been read. Note that this is the most efficient read, because up to
+   * {@code maximumEntries} {@code Entry<T>} instances will be answered. The {@code maximumEntries} should be used to indicate
+   * the total number of {@code Entry<T>} instances that can be consumed in a timely fashion by the sender,
+   * which is a natural back-pressure mechanism.
+   *
+   * @param fromId the String id of the {@code Entry<T>} instance to which the seek prepares to next read
+   * @param maximumEntries the int indicating the maximum number of {@code Entry<T>} instances to read
+   *
+   * @return the {@code Completes<List<T>>} of at most maximumEntries or empty if none
+   */
+  default Completes<List<T>> readNext(final String fromId, final int maximumEntries) {
+    seekTo(fromId);
+    return readNext(maximumEntries);
+  }
 
   /**
    * Rewinds the reader so that the next available {@code Entry<T>} is the first one in the storage.
@@ -106,24 +142,39 @@ public interface EntryReader<T extends Entry<?>> {
   Completes<String> seekTo(final String id);
 
   /**
+   * Eventually answer the size in {@code Entry} instances.
+   * @return {@code Completes<Long>}
+   */
+  Completes<Long> size();
+
+  /**
    * Provides advice for the specific implementation.
    */
   public static final class Advice {
     public final Object configuration;
     public final Class<? extends Actor> entryReaderClass;
+    public final String queryCount;
+    public final String queryLatestOffset;
     public final String queryEntryBatchExpression;
     public final String queryEntryExpression;
+    public final String queryUpdateCurrentOffset;
 
     public Advice(
             final Object configuration,
             final Class<? extends Actor> entryReaderClass,
             final String queryEntryBatchExpression,
-            final String queryEntryExpression) {
+            final String queryEntryExpression,
+            final String queryCount,
+            final String queryLatestOffset,
+            final String queryUpdateCurrentOffset) {
 
       this.configuration = configuration;
       this.entryReaderClass = entryReaderClass;
       this.queryEntryBatchExpression = queryEntryBatchExpression;
       this.queryEntryExpression = queryEntryExpression;
+      this.queryCount = queryCount;
+      this.queryLatestOffset = queryLatestOffset;
+      this.queryUpdateCurrentOffset = queryUpdateCurrentOffset;
     }
 
     @SuppressWarnings("unchecked")
