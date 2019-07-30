b411cc9ce85c00b5d161d1ab731b41804ec0ced4
VaughnVernon <vaughn@forcomprehension.com>
2019-07-29 17:54:19 -0700

    Implement typeName().

--- src/main/java/io/vlingo/symbio/BaseEntry.java
+++ src/main/java/io/vlingo/symbio/BaseEntry.java
@@ -93,7 +93,7 @@ public abstract class BaseEntry<T> implements Entry<T> {
 
   /* @see io.vlingo.symbio.Entry#type() */
   @Override
-  public String type() {
+  public String typeName() {
     return type;
   }
 
