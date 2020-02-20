398bdef275a7d4d5c4f26b702c66bb4220dd0736
VaughnVernon <vaughn@forcomprehension.com>
2020-02-20 06:09:02 +0100

    Initial revision of State and Object together.

---
+++ src/main/java/io/vlingo/symbio/StateBundle.java
@@ -0,0 +1,33 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.symbio;
+
+public class StateBundle {
+  public final State<?> state;
+  public final Object object;
+
+  public StateBundle(final State<?> state, final Object object) {
+    this.state = state;
+    this.object = object;
+  }
+
+  public StateBundle(final State<?> state) {
+    this.state = state;
+    this.object = null;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <S extends State<?>> S typedState() {
+    return (S) state;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <O extends Object> O typedObject() {
+    return (O) object;
+  }
+}
